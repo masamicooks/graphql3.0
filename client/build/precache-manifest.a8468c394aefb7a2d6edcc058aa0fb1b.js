003be3593f7d4c7c2865f8ffd7fa016ee3afea8f
Harrison Cramer <harrisoncramer@gmail.com>
2020-06-08 21:23:46 -0400

    reduced memory for react-scripts build https://stackoverflow.com/questions/44046366/increase-javascript-heap-size-in-create-react-app-project/55378108#55378108 (fixed)

---
+++ client/build/precache-manifest.a8468c394aefb7a2d6edcc058aa0fb1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad85f141ae519210011ab15c94150167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2cf42127a681b189ef0",
+    "url": "/static/css/2.24bf1742.chunk.css"
+  },
+  {
+    "revision": "e3d51b3dc7d75ccf3eb9",
+    "url": "/static/css/main.5ad2b496.chunk.css"
+  },
+  {
+    "revision": "c2cf42127a681b189ef0",
+    "url": "/static/js/2.beac6a62.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.beac6a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d51b3dc7d75ccf3eb9",
+    "url": "/static/js/main.ece40297.chunk.js"
+  },
+  {
+    "revision": "c71849e6b92a54d9b65d",
+    "url": "/static/js/runtime-main.dbac8e30.js"
+  }
+]);
