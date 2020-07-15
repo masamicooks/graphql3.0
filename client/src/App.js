b19bf2a115936af0097c55bb9439113f293d07f3
Harrison Cramer <harrisoncramer@gmail.com>
2020-07-15 14:58:27 -0400

    ga-key fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppRouter from "./routers/AppRouter";
 import ReactGA from "react-ga";
 
@@ -13,14 +13,6 @@ import "./styles.css";
 // Apollo Client + Provider
 import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./graphql/client";
-
-// Initialize Google Analytics
-function initializeReactGA() {
-  ReactGA.initialize(process.env.GOOGLE_ANALYTICS);
-  ReactGA.pageview("/");
-}
-
-initializeReactGA();
 
 // Assign isMobile property for styling
 const isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(
@@ -54,6 +46,12 @@ const theme = createMuiTheme({
 });
 
 function App() {
+  // Use Google Analytics
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS);
+    // To report page view...
+    ReactGA.pageview("/");
+  }, []);
   return (
     <ApolloProvider client={client}>
       <MuiThemeProvider theme={theme}>
