f18c439050cdf9fa83ae0af6d7f7dbc1598f978c
Harrison Cramer <harrisoncramer@gmail.com>
2020-07-15 14:12:01 -0400

    added google analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import AppRouter from "./routers/AppRouter";
+import ReactGA from "react-ga";
+
 import "normalize.css/normalize.css";
 
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
@@ -11,6 +13,12 @@ import "./styles.css";
 // Apollo Client + Provider
 import { ApolloProvider } from "@apollo/react-hooks";
 import client from "./graphql/client";
+
+// Initialize Google Analytics
+function initializeReactGA() {
+  ReactGA.initialize("UA-172772578-1");
+  ReactGA.pageview("/");
+}
 
 // Assign isMobile property for styling
 const isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(
